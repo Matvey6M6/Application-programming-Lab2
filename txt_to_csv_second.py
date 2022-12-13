11e207d9f465b66c1a7fdd238a1789b29cb4da44
Matvey6M6 <rolanddiskein666@mail.ru>
2022-12-12 21:43:01 -0600

    Remarks
    - Type hints
    - PEP8

--- txt_to_csv_second.py
+++ txt_to_csv_second.py
@@ -3,8 +3,13 @@ import os
 import shutil
 
 
-def write_as_csv(path_to_dataset, paths_to_files):
+def write_as_csv(path_to_dataset: str, paths_to_files: str):
+    """Writes as CSV 
 
+    Args:
+        path_to_dataset (_type_): path to source
+        paths_to_files (_type_): content from source
+    """
     with open("dataset_csv_first.csv", "w+", encoding='utf-8', newline='') as file:
         csv_file = csv.writer(file, delimiter=';')
         csv_file.writerow(["Absolute path", "Relative path", "Class"])
@@ -15,12 +20,24 @@ def write_as_csv(path_to_dataset, paths_to_files):
                               f'../application_programming_first_lab_and_dataset/dataset{path}', f'{path[1]}'])
 
 
-def mk_newdataset(nd_path):
+def mk_newdataset(nd_path: str):
+    """creates new dataset
+
+    Args:
+        nd_path (_type_): new dataset's path
+    """
     os.mkdir(nd_path)
 
 
-def copy_dataset(path_to_dataset):
+def copy_dataset(path_to_dataset: str) -> str:
+    """Copies dataset to the new direct
 
+    Args:
+        path_to_dataset (str): Path to old one
+
+    Returns:
+        str: Path to the new one
+    """
     nd_path='./new_dataset'
     mk_newdataset(nd_path)
 
@@ -34,21 +51,33 @@ def copy_dataset(path_to_dataset):
         for file_num in range(0, (num_of_files - 1)):
             shutil.copy(folder_path+f"/{(file_num+1):04}.txt", nd_path) #rewrite
             os.rename(f"./new_dataset/{(file_num+1):04}.txt", f"./new_dataset/{folder_num}_{(file_num+1):04}.txt") #rename
-
+    
     return nd_path
 
 
-def get_paths_to_files(path_to_dataset):
+def get_paths_to_files(path_to_dataset: str) -> str:
+    """Gets paths to files from dataset
+
+    Args:
+        path_to_dataset (str): Path to get files from
+
+    Returns:
+        str: List of paths
+    """
     paths_to_files = list()
+
     for folder_num in range(1, 6):
         folder_path = path_to_dataset+'/'+str(folder_num)
         num_of_files = sum(os.path.isfile(os.path.join(folder_path, f))
                            for f in os.listdir(folder_path)) + 1
+
         for file_num in range(1, num_of_files):
             path_to_file = folder_path+f'/{(file_num):04}'+'.txt'
             print(f'{folder_num} : {(file_num):04}')
             paths_to_files.append(path_to_file[len(path_to_dataset):])
+
     return paths_to_files
+
 
 if __name__ == '__main__':
 
